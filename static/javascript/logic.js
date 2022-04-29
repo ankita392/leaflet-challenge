9530e12071d380bef15c96a07279a1723cb44f91
ankita392 <91970314+ankita392@users.noreply.github.com>
2022-04-28 18:09:55 -0700

    Update logic.js

--- static/javascript/logic.js
+++ static/javascript/logic.js
@@ -1,6 +1,14 @@
-
 var earthquakes = new L.LayerGroup();
 var tectonicPlates = new L.LayerGroup();
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(map);
 
 var baseMaps = {
     "Satellite": satelliteMap,
