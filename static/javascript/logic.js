a7f82547b5e86a1ac9dc0aa5c6176d3e2f2de373
ankita392 <91970314+ankita392@users.noreply.github.com>
2022-04-28 18:14:36 -0700

    Update logic.js

--- static/javascript/logic.js
+++ static/javascript/logic.js
@@ -7,7 +7,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+    accessToken: APIKEY
 }).addTo(map);
 
 var baseMaps = {
